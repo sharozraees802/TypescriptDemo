e7936ef1ed719bb9c2b9e8f2dc324fbc5b766fe4
sharozraees802 <sharozraees802@gmail.com>
2021-08-26 22:42:07 +0500

    Createuser component add interface and function create app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -51,21 +51,55 @@
 // };
 
 // export default App;
-
+import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Header } from "./components/Header/Header";
 import CreateUser from "./components/CreateUser/CreateUser";
 import DisplayBoard from "./components/DisplayBoard/DisplayBoard";
+import { AppService } from "./services/app.service";
 
 const App = () => {
+  const [user, setUser] = useState<any>();
+  const [users, setUsers] = useState<any>();
+  const [numberofUsers, setNumberofUsers] = useState<number>(0);
+  const appServies = new AppService();
+
+  const createUser = (e: any) => {
+    const response = appServies.addUser(user);
+    console.log(response);
+    setNumberofUsers(numberofUsers + 1);
+  };
+
+  const getAllUsers = () => {
+    const users = appServies.getUser();
+    setUsers(users);
+    setNumberofUsers(users.length);
+  };
+
+  const onChangeFrom = (e: any) => {
+    let userClone = { ...user };
+    if (e.target.name === "firstname") {
+      userClone.firstName = e.target.value;
+    } else if (e.target.name === "lastname") {
+      userClone.lastName = e.target.value;
+    } else if (e.target.name === "email") {
+      userClone.email = e.target.value;
+    }
+    setUser(userClone);
+  };
+
   return (
     <div className="App">
       <Header />
       <div className="container mrgnbtm">
         <div className="row">
           <div className="col-md-8">
-            <CreateUser />
+            <CreateUser
+              user={user}
+              onChangeFrom={onChangeFrom}
+              createUser={createUser}
+            />
           </div>
 
           <div className="col-md-4">
--- src/components/CreateUser/CreateUser.tsx
+++ src/components/CreateUser/CreateUser.tsx
@@ -1,8 +1,13 @@
 
 import React from 'react'
 
+interface ICreateUser{
+    user:any;
+    onChangeFrom:Function;
+    createUser:Function;
+}
 
-const CreateUser = () => {
+const CreateUser = ({user,onChangeFrom,createUser}:ICreateUser) => {
 
 
     return(
@@ -14,20 +19,20 @@ const CreateUser = () => {
                     <div className="row">
                         <div className="form-group col-md-6">
                             <label htmlFor="exampleInputEmail1">First Name</label>
-                            <input type="text" className="form-control" name="firstname" id="firstname" aria-describedby="emailHelp" placeholder="First Name" />
+                            <input type="text" onChange={e=>onChangeFrom(e)} className="form-control" name="firstname" id="firstname" aria-describedby="emailHelp" placeholder="First Name" />
                         </div>
                         <div className="form-group col-md-6">
                             <label htmlFor="exampleInputPassword1">Last Name</label>
-                            <input type="text" className="form-control" name="lastname" id="lastname" placeholder="Last Name" />
+                            <input type="text" onChange={e=>onChangeFrom(e)} className="form-control" name="lastname" id="lastname" placeholder="Last Name" />
                         </div>
                     </div>
                     <div className="row">
                         <div className="form-group col-md-12">
                             <label htmlFor="exampleInputEmail1">Email</label>
-                            <input type="text" className="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Email" />
+                            <input type="text" onChange={e=>onChangeFrom(e)} className="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Email" />
                         </div>
                     </div>
-                    <button type="button" className="btn btn-danger mrgnbtm">Create</button>
+                    <button type="button" onClick={e=>createUser()} className="btn btn-danger mrgnbtm">Create</button>
                 </form>
                 </div>
             </div>
